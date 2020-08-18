c646e07fcc2f3de3902bfb8f4948263e7e5cd35d
shonaashton <shonaashton@gmail.com>
2020-08-18 23:05:33 +0100

    simplified video code - still only works on loading page and not for searches - js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function startPage() {
-  let city = "London";
+  let city = "london";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
   //the following code is to forecast weather
@@ -133,7 +133,7 @@ function localsearch(eventLocal) {
 //the following code changes the data and formating of the page to match the weather in the chosen city
 function showTemperature(response) {
   let icon = response.data.weather[0].icon;
-  console.log(icon);
+
   let temperature = Math.round(response.data.main.temp);
   let description = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
@@ -156,6 +156,8 @@ function showTemperature(response) {
   weatherSymbolElement.innerHTML = `<img
       src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}"
       />`;
+  video.innerHTML = `<source src="Videos/${icon}.mp4" />`;
+  console.log(icon);
   document.getElementById("celsius-btn").style.opacity = "100%";
   document.getElementById("fahrenheit-btn").style.opacity = "50%";
   isFahrenheitFunctionCalled = false;
@@ -167,97 +169,78 @@ function showTemperature(response) {
     bkgrdColor1 = "f9d423";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to right, #f83600 0%, #f9d423 100%)";
-
-    video.innerHTML = `<source src="Videos/clearsky.mp4" />`;
   } else if (icon === "01n") {
     sigColor = "ffffff";
     bkgrdColor1 = "161513";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to right, #243949 0%, #161513 100%)";
-    video.innerHTML = `<source src="Videos/clearskynight.mp4" />`;
   } else if (icon === "02d") {
     sigColor = "000000";
     bkgrdColor1 = "EA7A5C";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #E8F2AB 0%, #EA7A5C 100%)";
-    video.innerHTML = `<source src="Videos/fewclouds.mp4" />`;
   } else if (icon === "02n") {
     sigColor = "ffffff";
     bkgrdColor1 = "4403BC";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #a18cd1 0%, #2f2435 100%)";
-    video.innerHTML = `<source src="Videos/fewcloudsnight.mp4" />
-      `;
   } else if (icon === "03d" || icon === "04d") {
     sigColor = "000000";
     bkgrdColor1 = "a18cd1";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)";
-    video.innerHTML = `<source src="Videos/clouds.mp4" />`;
   } else if (icon === "03n") {
     sigColor = "ffffff";
     bkgrdColor1 = "6a85b6";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #6a85b6 0%, #23272e 100%)";
-    video.innerHTML = `<source src="Videos/cloudsnight.mp4" />`;
   } else if (icon === "04n") {
     sigColor = "ffffff";
     bkgrdColor1 = "33354d";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(-20deg,  #0f0f0f 0%, #33354d 100%)";
-    video.innerHTML = `<source src="Videos/cloudsnight.mp4" />`;
   } else if (icon === "09d" || icon === "10d") {
     sigColor = "000000";
     bkgrdColor1 = "ace0f9";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)";
-    video.innerHTML = `<source src="Videos/rain.mp4" type="video/mp4" />
-      `;
   } else if (icon === "09n" || icon === "10n") {
     sigColor = "ffffff";
     bkgrdColor1 = "32136B";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to right, #32136B 0%, #495aff 100%)";
-    video = document.querySelector("#video");
-    video.innerHTML = `<source src="Videos/rainnight.mp4" type="video/mp4" />`;
   } else if (icon === "11d" || icon === "11n") {
     sigColor = "ffffff";
     bkgrdColor1 = "b8235a";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #dbdcd7 0%, #dddcd7 24%, #e2c9cc 30%, #e7627d 46%, #b8235a 59%, #801357 71%, #3d1635 84%, #1c1a27 100%)";
-
-    video.innerHTML = `<source src="Videos/storm.mp4" type="video/mp4" />`;
   } else if (icon === "13d") {
     sigColor = "000000";
     bkgrdColor1 = "eef1f5";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%)";
-    video.innerHTML = `<source src="Videos/snow.mp4" />`;
   } else if (icon === "13n") {
     sigColor = "ffffff";
     bkgrdColor1 = "bcbcbc";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, lightgrey 0%, lightgrey 1%, #e0e0e0 26%, #efefef 48%, #d9d9d9 75%, #bcbcbc 100%)";
-    video.innerHTML = `<source src="Videos/snownight.mp4" />`;
   } else if (icon === "50d") {
     sigColor = "000000";
     bkgrdColor1 = "020f75";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to top, #fcc5e4 0%, #fda34b 15%, #ff7882 35%, #c8699e 52%, #d4d4b1 71%, #0c1db8 87%, #020f75 100%)";
-    video.innerHTML = `<source src="Videos/mist.mp4" />`;
   } else if (icon === "50n") {
     sigColor = "ffffff";
     bkgrdColor1 = "7046aa";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(-225deg, #231557 0%, #44107A 29%, #FF1361 67%, #7046aa 100%)";
-    video.innerHTML = `<source src="Videos/mistnight.mp4" />`;
   } else {
     sigColor = "000000";
     bkgrdColor1 = "eea2a2";
     document.getElementById("body").style.backgroundImage =
       "linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%)";
-    video.innerHTML = `<source src="Videos/sunsetclouds.mp4" />`;
   }
+
   document.getElementById("signature").style.color = `#${sigColor}`;
   document.getElementById(
     "current-day-and-time"
@@ -298,4 +281,5 @@ startPage();
 //decide what to do with pressure - convert it??
 // fix timezones - API is wrong not me
 //Add line-heights to things (line-height: 1; is the same as line-height: 64px; )
+//replace image names with icons so can call them using icon variable
 // https://ecstatic-bose-fe1b46.netlify.app/
